8612dd30842b8b30d83701eb6e47d96bded180f4
luzzy <luzzyzhang@gmail.com>
2017-02-17 16:00:02 +0800

    Create test tables

---
+++ the-computer-store/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Manufacturers (
+    Code INTEGER,
+    Name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Code)
+);
+
+CREATE TABLE Products (
+    Code INTEGER,
+    Name VARCHAR(255) NOT NULL,
+    Price DECIMAL NOT NULL,
+    Manufacturer INTEGER NOT NULL,
+    PRIMARY KEY (Code),
+    FOREIGN KEY (Manufacturer) REFERENCES Manufactures(Code)
+) ENGINE = INNODB;
